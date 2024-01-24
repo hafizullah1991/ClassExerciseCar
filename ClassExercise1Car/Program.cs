40a0e07883c1200e2d48ff343cadf2bb6bbfbbce
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-24 15:05:22 -0800

    class exercise on Car

---
+++ ClassExercise1Car/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Exercise1Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+            newCar.Make = "Tesla";
+            newCar.Model = "Hafiz";
+            newCar.Year = 2024;
+
+            Console.WriteLine($"{newCar.Make} {newCar.Model} {newCar.Year}");
+        }
+    }
+}
